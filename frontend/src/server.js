197eede4c2d1fab715d8ead0554821d5ccef5530
Alina <alina-yanchuk02@outlook.pt>
2020-06-03 23:27:07 +0100

    jjefef

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,10 @@
+
+var cors = require('cors');
+
 const path = require('path');
 const express = require('express');
 const app = express();
+app.use(cors());
 const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;app.use(express.static(publicPath));app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
@@ -8,7 +12,12 @@ const port = process.env.PORT || 3000;app.use(express.static(publicPath));app.ge
    console.log('Server is up!');
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
-var cors = require('cors')
 
-app.use(cors())
